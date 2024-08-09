29ae1c8785cf8532efced4f0b2d3976a2a21d70d
Amrwebprog <amr792134@gmail.com>
2024-08-09 22:58:23 +0300

    Initial commit

--- apps/script.js
+++ apps/script.js
@@ -4,7 +4,7 @@ let array = [
     price: 25,
     cat: 'burgers',
   },
-  
+
   {
     name: 'burger tow',
     price: 30,
@@ -99,6 +99,10 @@ let fatora = []
 let menue = false
 let fatoraDiv = document.querySelector('.fatora')
 let overlay = document.querySelector('.overlay')
+let vechile = document.querySelector('#vechile')
+let fatoraArray = {}
+let qty = 1
+let billdiv = document.querySelector('#bill')
 
 function renderCategory() {
   workSpace.innerHTML = ''
@@ -188,30 +192,33 @@ function products(cat) {
 }
 function addToFatora(product_Name, product_cat, product_img) {
   /*  let fatoraArray = arr.forEach((elem, index) => {}) */
-  let fatoraArray = arr[product_cat].filter((elem, index) => {
+  fatoraArray = arr[product_cat].filter((elem, index) => {
     return elem.name === product_Name && elem.cat === product_cat
   })
-
-  fatoraArray != 0 ? fatora.push(fatoraArray) : null
-  console.log(fatoraArray)
 
   sideMenue(fatoraArray, product_img)
 }
 function sideMenue(fatoraArray, product_img) {
+  qty = 1
   fatoraDiv.innerHTML = ``
   menue = !menue
   if (menue) {
     fatoraArray.forEach((elem, index) => {
       fatoraDiv.innerHTML = `
 
-    <div class="side-menue p-3 d-flex flex-row gap-1 ">
-    <div class=" d-flex flex-column justify-content-center h-100">
-    <div class="line"> </div>
+    <div class="side-menue d-flex flex-row  justify-content-center align-item-center">
+    <div class=" d-flex flex-column justify-content-center align-items-center h-100 col-1">
+    <div class="line"></div>
     </div>
-      <div class="d-flex flex-column justify-content-center bg-info p-3">
+      <div class="d-flex flex-column justify-content-center align-items-center bg-info p-3 col-11">
       <img class="col-12" src="assets/img/${elem.cat + product_img}.jpg">
-      <h1>${elem.price}</h1>
+      <h1>${elem.name}</h1>
+       <div class="d-flex flex-row col-12 d-flex flex-row col-12 justify-content-center align-items-center gap-2">
+      <button id="button" class="col-3" onclick="removeqty(${product_img})">-</button> <h2>${qty}</h2> <button id="button" class="col-3" onclick="Addqty(${product_img})">+</button>
+      </div>
+      <h2>${elem.price}</h2>
 
+      <button  id="button" class="btn btn-warning" onclick="addItTovechile()"> Add it To bill </button>
       </div>
     </div>
 
@@ -220,5 +227,94 @@ function sideMenue(fatoraArray, product_img) {
     overlay.classList.replace('d-none', 'd-block')
   } else {
     overlay.classList.replace('d-block', 'd-none')
+    billdiv.classList.replace('d-block', 'd-none')
   }
+}
+function addItTovechile() {
+  fatoraArray != 0 ? fatora.push(...fatoraArray) : null
+  overlay.classList.replace('d-block', 'd-none')
+  vechile.classList.replace('d-none', 'd-flex')
+  fatoraDiv.innerHTML = ``
+  vechile.innerHTML = `
+  <i class="fa-solid fa-cart-shopping"><p class="position-absolute top-0 end-50 bg-danger p-1 rounded-circle fs-6 ">${fatora.length}</p></i>
+  `
+  console.log(fatora)
+}
+function Addqty(product_img) {
+  qty = qty + 1
+  fatoraArray[0].originalPrice = fatoraArray[0].price * qty
+  fatoraArray[0].qty = qty
+  console.log(fatoraArray)
+  fatoraArray.forEach((elem, index) => {
+    fatoraDiv.innerHTML = `
+       <div class="side-menue d-flex flex-row  justify-content-center align-item-center">
+    <div class=" d-flex flex-column justify-content-center align-items-center h-100 col-1">
+    <div class="line"></div>
+    </div>
+      <div class="d-flex flex-column justify-content-center align-items-center bg-info p-3 col-11">
+      <img class="col-12" src="assets/img/${elem.cat + product_img}.jpg">
+      <h1>${elem.name}</h1>
+       <div class="d-flex flex-row col-12 justify-content-center align-items-center gap-2">
+      <button id="button" class="col-3" onclick="removeqty(${product_img})">-</button>
+       <h2>${qty}</h2>
+       <button id="button" class="col-3" onclick="Addqty(${product_img})">+</button>
+      </div>
+      <h2>${elem.price * qty}</h2>
+
+      <button class="btn btn-warning" onclick="addItTovechile()"> Add it To bill </button>
+      </div>
+    </div>
+      `
+  })
+}
+function removeqty(product_img) {
+  if (qty < 1) {
+  } else {
+    qty = qty - 1
+    fatoraArray[0].originalPrice = fatoraArray[0].price * qty
+    console.log(fatoraArray)
+    fatoraArray.forEach((elem, index) => {
+      fatoraDiv.innerHTML = `
+       <div class="side-menue d-flex flex-row  justify-content-center align-item-center">
+    <div class=" d-flex flex-column justify-content-center align-items-center h-100 col-1">
+    <div class="line"></div>
+    </div>
+      <div class="d-flex flex-column justify-content-center align-items-center bg-info p-3 col-11">
+      <img class="col-12" src="assets/img/${elem.cat + product_img}.jpg">
+      <h1>${elem.name}</h1>
+       <div class="d-flex flex-row col-12 justify-content-center align-items-center gap-2">
+      <button id="button" class="col-3" onclick="removeqty(${product_img})">-</button>
+       <h2>${qty}</h2>
+       <button id="button" class="col-3" onclick="Addqty(${product_img})">+</button>
+      </div>
+      <h2>${elem.price * qty}</h2>
+
+      <button class="btn btn-warning" onclick="addItTovechile()"> Add it To bill </button>
+      </div>
+    </div>
+      `
+    })
+  }
+}
+function Bill() {
+  console.log(fatoraArray)
+  console.log(fatora)
+  billdiv.classList.replace('d-none', 'd-block')
+  overlay.classList.replace('d-none', 'd-block')
+  billdiv.innerHTML = ``
+  fatora.forEach((elem, index) => {
+    billdiv.innerHTML += `
+
+  <div
+        id="product"
+        class="col-12 d-flex flex-column align-items-center gap-4 bg-warning bg-gradient"
+      >
+        <img src="./assets/img/.jpg" alt="" />
+        <h1>${elem.name}</h1>
+        <h5>${elem.price}</h5>
+        <h5>${elem.qty}</h5>
+        <h5>${elem.originalPrice}</h5>
+      </div>
+       `
+  })
 }
